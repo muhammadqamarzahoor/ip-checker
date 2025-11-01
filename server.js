fb107637077070366d4b7131355fd6562bd12778
Muhammad Qamar <engrqamarzahoor@gmail.com>
2025-11-01 17:34:56 +0500

    remove lowdb and switch to supabase

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ import { fileURLToPath } from "url";
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 dotenv.config();
+console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+console.log(
+  "SUPABASE_SERVICE_ROLE_KEY:",
+  process.env.SUPABASE_SERVICE_ROLE_KEY ? "✅ Loaded" : "❌ Missing"
+);
 
 const app = express();
 
